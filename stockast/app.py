49aa53fd26113378feb6bf06433de3aa31e2fadf
itomaldonado <mo.maldonado@gmail.com>
2019-04-29 20:53:34 -0400

    added and enabled cors

--- stockast/app.py
+++ stockast/app.py
@@ -2,6 +2,7 @@ import falcon
 import logging
 
 from falcon_autocrud.middleware import Middleware
+from falcon_cors import CORS
 from sqlalchemy import create_engine
 
 from stockast import config
@@ -13,12 +14,19 @@ from stockast.resources import user
 
 logger = logging.getLogger(__name__)
 
+# CORS
+cors = CORS(
+    allow_all_origins=True,
+    allow_all_headers=True,
+    allow_all_methods=True,
+    allow_credentials_all_origins=True)
+
 # database initialization
 engine = create_engine(config.DATABASE_URL, echo=config.DATABASE_DEBUG)
 Base.metadata.create_all(engine)
 
 # falcon.API instances are callable WSGI apps
-application = falcon.API(middleware=[Middleware()])
+application = falcon.API(middleware=[Middleware(), cors.middleware])
 
 # paths and resources
 application.add_route(
